Zeus-OS/android_frameworks_av

cameraservice: move client_package_name to data/misc
Change-Id: I0d244b81625fd9514000bea313a28ad60321333f

services/camera/libcameraservice/CameraService.cpp
@@ -2528,7 +2528,7 @@ status_t CameraService::BasicClient::startCameraOps() {
     sCameraService->mUidPolicy->registerMonitorUid(mClientUid);
 
 #ifdef TARGET_NEEDS_CLIENT_INFO
-    std::ofstream cpf("/data/vendor/zenx/client_package_name");
+    std::ofstream cpf("/data/misc/zenx/client_package_name");
     std::string cpn = String8(mClientPackageName).string();
     cpf << cpn;
 #endif

R15Hi/platform_frameworks_av

services/camera/libcameraservice/CameraService.cpp
@@ -2537,7 +2537,7 @@ status_t CameraService::BasicClient::startCameraOps() {
     sCameraService->updateOpenCloseStatus(mCameraIdStr, true/*open*/, mClientPackageName);
 
     #ifdef TARGET_NEEDS_CLIENT_INFO
-    std::ofstream cpf("/data/vendor/octavi/client_package_name");
+    std::ofstream cpf("/data/misc/octavi/client_package_name");
     std::string cpn = String8(mClientPackageName).string();
     cpf << cpn;
 #endif